03bacc512b4517df3da9fac4d0d510e5f6e19df7
Bozey98 <dmusatov98@gmail.com>
2017-10-29 22:25:39 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,27 +1,34 @@
 #include <codecvt>
 #include <iostream>
 
-#include "palindrome.hpp"
+#include "Stack1.hpp"
 
-auto utf8string_towstring(std::string const & input) -> std::wstring
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    return converter.from_bytes( input.c_str() );
-}
-
-int main()
-{
-    for( std::string string; std::getline( std::cin, string ); ) {
-        
-        for( size_t i = 0; i < string.size(); ++i ) {
-            std::cout << std::bitset<sizeof(char) * 8>( string[ i ] ) << std::endl;
-        }
-        
-        if( palindrome( utf8string_towstring(string), std::locale("ru_RU.UTF-8") ) ) {
-            std::cout << "1\n";
-        }
-        else {
-            std::cout << "0\n";
-        }
-    }
+int main() {
+	Stack<int> Example;
+	char c;
+	cout << "Add an element: +element\nShow the last element: ?\nPop the last element: -\nShow the stack: =\nExit: q\n\n";
+	while (cin >> c && c != 'q')
+	{
+		int i;
+		switch (c)
+		{
+		case '+':
+		{
+			cin >> i;
+			Example.push(i);
+			break;
+		}
+		case '-':
+			Example.pop();
+			break;
+		case '=':
+			Example.print();
+			break;
+		case '?':
+			cout<< Example.last()<<endl;
+			break;
+		}
+	}
+	system("pause");
+	return 0;
 }
